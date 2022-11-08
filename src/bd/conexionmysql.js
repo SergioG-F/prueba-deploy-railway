3053b9e1fa7d8efb4071c4469c3876c7d296e0b7
sergioG-F <sguzmanfe@proveedor.intercorp.com.pe>
2022-11-08 16:35:29 -0500

    16to cambio

--- src/bd/conexionmysql.js
+++ src/bd/conexionmysql.js
@@ -1,11 +1,18 @@
 const {mysql} = require('mysql2/promise');
 const {DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,DB_PORT} = require('../config/bd/configbd.js');
 const  poolmysql =  mysql.createPool ( { 
-    host:DB_HOST,
-    user:DB_USER,
-    password:DB_PASSWORD,
-    database:DB_NAME,
-    port:DB_PORT,
+    // host:DB_HOST,
+    // user:DB_USER,
+    // password:DB_PASSWORD,
+    // database:DB_NAME,
+    // port:DB_PORT,
+
+    host:'containers-us-west-86.railway.app',
+    user:'root',
+    password:'vR4JgrPuOQg94b9FpeVP',
+    database:'railway',
+    port:'7766',
+
     multipleStatements: true
   } ) ;
   console.log("BADE DE DATOS  MYSQL ONLINE");
