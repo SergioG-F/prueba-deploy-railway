b3d873f29b797c44bb2305238e79db4e9c45da84
sergioG-F <sguzmanfe@proveedor.intercorp.com.pe>
2022-11-08 16:24:54 -0500

    14to cambio

--- src/bd/conexionmysql.js
+++ src/bd/conexionmysql.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql2/promise');
+const {pool} = require('mysql2/promise');
 const {DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,DB_PORT} = require('../config/bd/configbd.js');
-const  poolmysql =  mysql.createPool ( { 
+const  poolmysql =  pool.createPool ( { 
     host:DB_HOST,
     user:DB_USER,
     password:DB_PASSWORD,
