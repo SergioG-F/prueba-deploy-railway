c47058d9927227faf2aa9db87877973bfe7ee695
sergioG-F <guzmanfernandezsergio@gmail.com>
2022-11-08 22:03:14 -0500

    26to Update

--- src/bd/conexionmysql.js
+++ src/bd/conexionmysql.js
@@ -1,12 +1,23 @@
-const mysql = require('mysql2');
+// obtener el cliente 
+const  mysql  =  require ( 'mysql2/promise' ) ;
+// obtenga la implementación de la promesa, usaremos bluebird 
+const  bluebird  =  require ( 'bluebird' ) ;
+
+
+// crea la conexión, especifica bluebird como Promise 
+
+// consulta base de datos 
+//const [rows, fields] = await connection.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14]);
+
+
 const {DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,DB_PORT} = require('../config/bd/configbd.js');
-const  pool =  mysql.createPool( { 
+const  pool = await  mysql.createPool( { 
     host:DB_HOST,
     user:DB_USER,
     password:DB_PASSWORD,
     database:DB_NAME,
     port:DB_PORT,
-
+    Promise: bluebird,
     // host:'containers-us-west-86.railway.app',
     // user:'root',
     // password:'vR4JgrPuOQg94b9FpeVP',
@@ -16,7 +27,5 @@ const  pool =  mysql.createPool( {
     multipleStatements: true
   } ) ;
   console.log("BADE DE DATOS  MYSQL ONLINE");
-
-  
 
 module.exports = pool;
