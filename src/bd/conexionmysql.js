fc7be36a119b5f8734bdf9efd5419a23ebf14e2d
sergioG-F <guzmanfernandezsergio@gmail.com>
2022-11-08 21:27:02 -0500

    23to cambio

--- src/bd/conexionmysql.js
+++ src/bd/conexionmysql.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const {DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,DB_PORT} = require('../config/bd/configbd.js');
-const  poolmysql =  mysql.createPool ( { 
+const  poolmysql =  mysql.createConnection ( { 
     host:DB_HOST,
     user:DB_USER,
     password:DB_PASSWORD,
